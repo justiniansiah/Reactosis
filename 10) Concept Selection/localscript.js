893be7761468928aa8759a123ed444c0d5252651
justiniansiah <justinian_siah@mymail.sutd.edu.sg>
2018-03-11 01:55:07 +0800

    added requirements allocation styles

---
+++ 10) Concept Selection/localscript.js
@@ -0,0 +1,51 @@
+/* Function for bd
+********************************************/
+function bd_in(){
+    var a = document.getElementsByClassName("bd");
+    var i;
+    for (i = 0; i < a.length; i++) {
+      a[i].style.background = "#fbed6a";
+    }
+}
+
+function bd_out(){
+    var a = document.getElementsByClassName("bd");
+    var i;
+    for (i = 0; i < a.length; i++) {
+      a[i].style.background = "#fefad6";
+    }
+}
+
+function ad_in(){
+    var a = document.getElementsByClassName("ad");
+    var i;
+    for (i = 0; i < a.length; i++) {
+      a[i].style.background = "#fbed6a";
+    }
+}
+
+function ad_out(){
+    var a = document.getElementsByClassName("ad");
+    var i;
+    for (i = 0; i < a.length; i++) {
+      a[i].style.background = "#fefad6";
+    }
+}
+
+function ld_in(){
+    var a = document.getElementsByClassName("ld");
+    var i;
+    for (i = 0; i < a.length; i++) {
+      a[i].style.background = "#fbed6a";
+    }
+}
+
+function ld_out(){
+    var a = document.getElementsByClassName("ld");
+    var i;
+    for (i = 0; i < a.length; i++) {
+      a[i].style.background = "#fefad6";
+    }
+}
+
+
